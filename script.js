092487bcfd50440087778aa51cb2866c7da89c9c
Vijaykumar018 <vijaykumarh0620@gmail.com>
2026-05-07 20:29:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://platform-copilot-pevl.vercel.app/';
 let currentIntent = null;
 let conversationHistory = [];
 let currentCategory = null;
